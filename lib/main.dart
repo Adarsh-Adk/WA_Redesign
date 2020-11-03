339c93ce97fd8fc293a71018cd30470dd61018b6
GHOST <adarshkakkat7@gmail.com>
2020-11-03 16:19:09 +0530

    initial ugly build

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_demo/HomeUi/Home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
