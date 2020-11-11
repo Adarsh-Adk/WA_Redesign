cf988ec11f56036f9d47e4c5a0199b1359b44bef
GHOST <adarshkakkat7@gmail.com>
2020-11-11 22:03:20 +0530

    implemented status

--- lib/Status/Statuses.dart
+++ lib/Status/Statuses.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:whatsapp_demo/Constants/Constants.dart';
-import 'package:dotted_border/dotted_border.dart';
-import 'package:whatsapp_demo/Status/Statuses.dart';
+import 'package:story_view/controller/story_controller.dart';
+import 'package:story_view/story_view.dart';
 import 'Status.dart';
 
- final deGreen= Constants().deGreen;
+class Statuses extends StatefulWidget {
+  @override
+  _StatusesState createState() => _StatusesState();
+}
 
-class StatusView extends StatelessWidget {
+class _StatusesState extends State<Statuses> {
+  final StoryController storyController = StoryController();
+
   final List<Status> _status = [
     Status(
         "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
@@ -47,47 +51,38 @@ class StatusView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 106,
-        child: ListView(
-          scrollDirection: Axis.horizontal,
-          shrinkWrap: true,
-          children: _status.map((status) {
-            return InkWell(
-              onTap: (){Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Statuses()));},
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 5,horizontal: 7),
-                    child: DottedBorder(
-                      color: deGreen,
-                      borderType: BorderType.Circle,
-                      radius: Radius.circular(55),
-                      strokeWidth: 3,
-                      dashPattern: [200],
-                      child: Container(
-                        padding: EdgeInsets.all(
-                          2,
-                        ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(55),
-                          child: Image(
-                            image: NetworkImage(status.image),
-                            width: 55,
-                            height: 55,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(status.name),
-                ],
-              ),
-            );
-          }).toList(),
-        ));
+      child: StoryView(
+        controller: storyController,
+        storyItems: [
+          StoryItem.inlineImage(
+              url:
+                  "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+              caption: null,
+              controller: storyController),
+          StoryItem.inlineImage(
+              url:
+                  "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+              caption: null,
+              controller: storyController),
+          StoryItem.inlineImage(
+              url:
+                  "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+              caption: null,
+              controller: storyController),
+          StoryItem.inlineImage(
+              url:
+                  "https://images.pexels.com/photos/1124724/pexels-photo-1124724.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+              caption: null,
+              controller: storyController),
+          StoryItem.inlineImage(
+              url:"https://images.pexels.com/photos/1832959/pexels-photo-1832959.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+              caption: null,
+              controller: storyController),
+        ],
+        onComplete: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
   }
 }
